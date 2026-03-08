9050cf81a7a21fc4e4f4470238419a1d7fd0d3bd
alibal <ali2005bal@gmail.com>
2026-03-08 21:30:08 +0300

    update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://pledgepay-production.up.railway.app',
 });
 
 // Her istekte token otomatik ekle
